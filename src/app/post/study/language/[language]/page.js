eefec097a83ce23a1221d4255f99ab252b279b9a
Ellie <dpfla9144@naver.com>
2023-11-03 11:14:14 +0900

    data fetch - post/study/language

--- src/app/post/study/language/[language]/page.js
+++ src/app/post/study/language/[language]/page.js
@@ -1,7 +1,12 @@
-export default function Language() {
+export default async function Language(props) {
+  const response = await fetch(
+    `https://myblog-3ce64-default-rtdb.firebaseio.com/post/study/language/${props.params.language}.json`
+  );
+  const result = await response.json();
   return (
     <>
-      <div>Language</div>
+      <h1>{result.title}</h1>
+      <div>{result.description}</div>
     </>
   );
 }
