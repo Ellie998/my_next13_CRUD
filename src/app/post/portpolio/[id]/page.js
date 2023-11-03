b9bb65c57d0a00ff4a826a2d1032c4eda8e714ad
Ellie <dpfla9144@naver.com>
2023-11-03 11:09:52 +0900

    data fetch - post/portpolio

--- src/app/post/portpolio/[id]/page.js
+++ src/app/post/portpolio/[id]/page.js
@@ -1,7 +1,16 @@
-export default function Portpolio() {
+export default async function Portpolio(props) {
+  const response = await fetch(
+    `https://myblog-3ce64-default-rtdb.firebaseio.com/post/portpolio/${props.params.id}.json`
+  );
+  const result = await response.json();
+
   return (
     <>
-      <div>Portpolio</div>
+      <h1>{result.title}</h1>
+      <div>{result.description}</div>
+      <a href={result.url} target="_blank">
+        {result.url}
+      </a>
     </>
   );
 }
