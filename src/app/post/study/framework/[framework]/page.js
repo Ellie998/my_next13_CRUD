9f8c7f02f1caf98449961df08feb8dea92166d57
Ellie <dpfla9144@naver.com>
2023-11-04 15:59:11 +0900

    use enviroment variable

--- src/app/post/study/framework/[framework]/page.js
+++ src/app/post/study/framework/[framework]/page.js
@@ -1,6 +1,6 @@
 export default async function Framework(props) {
   const response = await fetch(
-    `https://myblog-3ce64-default-rtdb.firebaseio.com/post/study/library&framework/${props.params.framework}.json`
+    `${process.env.NEXT_PUBLIC_URL_API}/post/study/library&framework/${props.params.framework}.json`
   );
   const result = await response.json();
   return (
