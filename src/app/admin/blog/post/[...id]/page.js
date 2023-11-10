845f8a65c6519fea6b76eb41f3631c2c78e9f0ff
Ellie <dpfla9144@naver.com>
2023-11-10 14:39:31 +0900

    reorganize dynamic folders

--- src/app/admin/blog/post/[...id]/page.js
+++ src/app/admin/blog/post/[...id]/page.js
@@ -1,20 +1,22 @@
 import ControlPost from "@/components/blog/ControlPost";
 
-export default async function Portpolio(props) {
+export default async function Posts(props) {
   const response = await fetch(
-    `https://myblog-3ce64-default-rtdb.firebaseio.com/post/portpolio/${props.params.id}.json`,
+    `${process.env.NEXT_PUBLIC_URL_API}/post/${props.params.id[0]}/${
+      props.params.id[1]
+    }${props.params.id[2] === undefined ? "" : "/" + props.params.id[2]}.json`,
     { cache: "no-cache" }
   );
   const result = await response.json();
 
   return (
-    <>
+    <div>
       <h1>{result.title}</h1>
       <div>{result.description}</div>
       <a href={result.url} target="_blank">
         {result.url}
       </a>
       <ControlPost url={`portpolio/${props.params.id}`} />
-    </>
+    </div>
   );
 }
